669e0b8e651983711fadfd36747b0ea9fe2b31c4
Tom <yangg@shinetechchina.com>
2017-06-02 18:08:06 +0800

    learning create a view model

---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Models/viewModels/DefaultPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using EpiserverSiteWithEmpty.Business;
+using EpiserverSiteWithEmpty.Models.Pages;
+using EPiServer.Core;
+
+namespace EpiserverSiteWithEmpty.Models.viewModels
+{
+    public class DefaultPageViewModel <T> :IPageViewModel<T> where T:SitePageData
+    {
+        public DefaultPageViewModel(T currentPage)
+        {
+            CurrentPage = currentPage;
+            Section = ContentExtensions.GetSection(currentPage.ContentLink);
+        }
+
+        public T CurrentPage { get; set; }
+        public IContent Section { get; set; }
+    }
+}
---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Models/viewModels/IPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using EpiserverSiteWithEmpty.Models.Pages;
+
+namespace EpiserverSiteWithEmpty.Models.viewModels
+{
+    public interface IPageViewModel<out T> where T:SitePageData
+    {
+        T CurrentPage { get; }
+    }
+}
