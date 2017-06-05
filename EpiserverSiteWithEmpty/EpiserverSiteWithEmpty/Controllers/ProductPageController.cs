0f72c7750a105c2153761b2220f8332c095113f4
Tom <yangg@shinetechchina.com>
2017-06-05 17:44:57 +0800

    exercise B5 Creating a new layout page, a product page type and a product controller and view

---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Controllers/ProductPageController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using EpiserverSiteWithEmpty.Models.Pages;
+using EpiserverSiteWithEmpty.Models.viewModels;
+using EPiServer.Web.Mvc;
+
+namespace EpiserverSiteWithEmpty.Controllers
+{
+    public class ProductPageController : PageControllerBase<ProductPage>
+    {
+        public ActionResult Index(ProductPage currentPage)
+        {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+             var model=new DefaultPageViewModel<ProductPage>(currentPage);
+            return View(model);
+        }
+    }
+}
---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Models/Media/ImageFile.cs
@@ -0,0 +1,21 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+
+namespace EpiserverSiteWithEmpty.Models.Media
+{
+    [ContentType(DisplayName = "ImageFile", GUID = "483a4f58-82fd-4e7f-95dd-8c50f7fd920a", Description = "")]
+    /*[MediaDescriptor(ExtensionString = "pdf,doc,docx")]
+      ImageData instead of MediaData
+         */
+    public class ImageFile : ImageData
+    {
+               /* [CultureSpecific]
+                [Editable(true)]
+                [Display(
+                    Name = "Description",
+                    Description = "Description field's description",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]*/
+                public virtual string Description { get; set; }
+    }
+}
