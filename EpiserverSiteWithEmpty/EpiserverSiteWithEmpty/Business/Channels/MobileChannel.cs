74b1bc3484487a7ebf5b2b8b76bc390ca9542a9c
Tom <yangg@shinetechchina.com>
2017-06-06 18:09:20 +0800

    exercise C4 (optional) create display channel for mobile

---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Business/Channels/MobileChannel.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+using System.Web;
+using EPiServer.Web;
+
+namespace EpiserverSiteWithEmpty.Business.Channels
+{
+    public class MobileChannel : DisplayChannel
+    {
+        public MobileChannel()
+        {
+            ResolutionId = typeof(MobileResolution).FullName;
+            ChannelName = "Mobile";
+        }
+
+        public override bool IsActive(HttpContextBase context)
+        {
+            return true;
+        }
+
+        public override string ChannelName { get; }
+
+        public override string ResolutionId { get; }
+    }
+}
---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Business/Channels/MobileResolution.cs
@@ -0,0 +1,20 @@
+﻿using EPiServer.Web;
+
+namespace EpiserverSiteWithEmpty.Business.Channels
+{
+    public class MobileResolution:IDisplayResolution
+    {
+        public MobileResolution()
+        {
+            Id = this.GetType().FullName;
+            Name = "Mobile(320X480)";
+            Width = 320;
+            Height = 480;
+        }
+
+        public string Id { get; }
+        public string Name { get; }
+        public int Width { get; }
+        public int Height { get; }
+    }
+}
