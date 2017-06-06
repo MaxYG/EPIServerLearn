0af6cfb39bcffd0340c987ccb39eae4eed8e7134
Tom <yangg@shinetechchina.com>
2017-06-06 15:00:28 +0800

    exercise c3 tag the partial template to only render in tagged content area

---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Controllers/PagePartialWideController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using EpiserverSiteWithEmpty.Models.Pages;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+
+namespace EpiserverSiteWithEmpty.Controllers
+{
+    [TemplateDescriptor(Inherited = true, Tags = new string[] {"span8"}, AvailableWithoutTag = false)]
+    public class PagePartialWideController : PartialContentController<SitePageData>
+    {
+        // GET: PagePartialWide
+        public override ActionResult Index(SitePageData currentContent)
+        {
+            return PartialView("/Views/Shared/PagePartials/PagePartialWide.cshtml", currentContent);
+        }
+    }
+}
--- EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Models/Pages/ProductPage.cs
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Models/Pages/ProductPage.cs
@@ -8,18 +8,22 @@ namespace EpiserverSiteWithEmpty.Models.Pages
     [ContentType(DisplayName = "ProductPage", GUID = "c5cd9da4-962b-4c23-b9e7-7e813042afd8", Description = "Alloy product page")]
     public class ProductPage : StandardPage
     {
-        /* [CultureSpecific]
-         [Display(
-             Name = "Main body",
-             Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
-             GroupName = SystemTabNames.Content,
-             Order = 1)]
-         public virtual XhtmlString MainBody { get; set; }*/
-
         [Display(
                 GroupName = SystemTabNames.Content,
                 Order = 305)]
         [UIHint("Textarea")]
         public virtual string UniqueSellingPoints { get; set; }
+
+        [Display(
+                GroupName = SystemTabNames.Content,
+                Order = 320)]
+        public virtual ContentArea MainContentArea { get; set; }
+
+        [Display(
+                GroupName = SystemTabNames.Content,
+                Order = 330)]
+        public virtual ContentArea RelatedContentArea { get; set; }
+
+
     }
 }
