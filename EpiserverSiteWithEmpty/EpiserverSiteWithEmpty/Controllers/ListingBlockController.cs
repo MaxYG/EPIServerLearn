52fc362e1192ec555056da0969721ce27e2339c3
Tom <yangg@shinetechchina.com>
2017-06-07 16:36:05 +0800

    exercise E1 create a page listing block

---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Controllers/ListingBlockController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EpiserverSiteWithEmpty.Models.Blocks;
+using EpiserverSiteWithEmpty.Models.ViewModels;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+using EPiServer.Web.Mvc;
+
+namespace EpiserverSiteWithEmpty.Controllers
+{
+    public class ListingBlockController : BlockController<ListingBlock>
+    {
+        public override ActionResult Index(ListingBlock currentBlock)
+        {
+            var contentRepository = ServiceLocator.Current.GetInstance<IContentRepository>();
+            var model = new ListingBlockModel
+            {
+                Heading = currentBlock.Heading,
+                Items = currentBlock.RootPage != null
+                    ? contentRepository.GetChildren<PageData>(currentBlock.RootPage)
+                    : null
+            };
+            return PartialView(model);
+        }
+    }
+}
---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Models/Blocks/ListingBlock.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EpiserverSiteWithEmpty.Models.Blocks
+{
+    [ContentType(DisplayName = "ListingBlock", GUID = "a1b15243-80d5-42d5-bbb7-17d25b49b9ff", Description = "")]
+    public class ListingBlock : BlockData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Name",
+                    Description = "Name field's description",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual string Name { get; set; }
+         */
+
+        [Display(
+                    Name = "Heading",
+                    Description = "listing block heading",
+                    GroupName = SystemTabNames.Content,
+                    Order = 100)]
+        public virtual string Heading { get; set; }
+
+        [Display(
+                   Name = "RootPage",
+                   Description = "listing block rootpage",
+                   GroupName = SystemTabNames.Content,
+                   Order = 200)]
+        public virtual PageReference RootPage { get; set; }
+    }
+}
