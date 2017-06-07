bcc3eada62ae1dbc93fb6b6ef1d68ab7eb7df32f
Tom <yangg@shinetechchina.com>
2017-06-07 10:32:51 +0800

    exercise D1 create a block without a controller

---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Models/Blocks/EditorialBlock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EpiserverSiteWithEmpty.Models.Blocks
+{
+    [ContentType(DisplayName = "EditorialBlock", GUID = "f10f2ecc-466c-4ef2-bb27-b8a45c9870ff", Description = "")]
+    public class EditorialBlock : BlockData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Name",
+                    Description = "Name field's description",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual string Name { get; set; }
+         */
+
+        [CultureSpecific]
+        [Display(
+                    Name = "Block main body",
+                    Description = "Block main body description",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
