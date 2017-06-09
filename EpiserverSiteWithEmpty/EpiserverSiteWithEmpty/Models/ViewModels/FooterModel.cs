2598eebba63f26fde4877179a098b50914785450
Tom <yangg@shinetechchina.com>
2017-06-09 11:08:24 +0800

    exercise F2 create footer using FindPagesWithCriteria, translation & login functionality

---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Models/ViewModels/FooterModel.cs
@@ -0,0 +1,51 @@
+﻿using EpiserverSiteWithEmpty.Models.Pages;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.Filters;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Routing;
+
+
+namespace EpiserverSiteWithEmpty.Models.ViewModels
+{
+    public class FooterModel
+    {
+        public bool LoggedIn { get; set; }
+        public PageDataCollection ProductPages { get; set; }
+        public string LoginUrl { get; set; }
+
+        public FooterModel(SitePageData currentPage)
+        {
+            this.LoginUrl = GetLoginUrl(currentPage.ContentLink);
+            this.LoggedIn = System.Web.HttpContext.Current.User.Identity.IsAuthenticated;
+            RetrieveSiteProductPages();
+        }
+
+        private void RetrieveSiteProductPages()
+        {
+            PropertyCriteriaCollection criteria = new PropertyCriteriaCollection();
+
+            PropertyCriteria prodpagecriterion = new PropertyCriteria();
+            prodpagecriterion.Condition = CompareCondition.Equal;
+            prodpagecriterion.Name = "PageTypeID";
+            prodpagecriterion.Type = PropertyDataType.PageType;
+
+            var contentTypeRepository = ServiceLocator.Current.GetInstance<IContentTypeRepository>();
+            prodpagecriterion.Value = contentTypeRepository.Load("ProductPage").ID.ToString();
+            prodpagecriterion.Required = true;
+            criteria.Add(prodpagecriterion);
+
+            ProductPages = DataFactory.Instance.FindPagesWithCriteria(PageReference.StartPage, criteria);
+        }
+
+        private string GetLoginUrl(ContentReference returnToContentLink)
+        {
+            var urlResolver = ServiceLocator.Current.GetInstance<UrlResolver>();
+            var returnUrl = urlResolver.GetUrl(returnToContentLink);
+            return returnUrl;
+        }
+
+       
+    }
+}
