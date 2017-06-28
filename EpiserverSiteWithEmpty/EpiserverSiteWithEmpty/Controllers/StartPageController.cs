f20a594db52ab722ac2a9649e4e729dd6b7fd39c
Tom <yangg@shinetechchina.com>
2017-06-28 09:43:00 +0800

    format and add note

--- EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Controllers/StartPageController.cs
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Controllers/StartPageController.cs
@@ -2,16 +2,12 @@
 using EpiserverSiteWithEmpty.Models.Pages;
 using EpiserverSiteWithEmpty.Models.ViewModels;
 
-
 namespace EpiserverSiteWithEmpty.Controllers
 {
     public class StartPageController : PageControllerBase<StartPage>
     {
         public ActionResult Index(StartPage currentPage)
         {
-            /* Implementation of action. You can create your own view model class that you pass to the view or
-             * you can pass the page type for simpler templates */
-
             var defaultPageViewModel = new DefaultPageViewModel<StartPage>(currentPage);
             return View(defaultPageViewModel);
         }
