0f5f09b6167a41a3bc76c351a928de361829f0f4
Tom <yangg@shinetechchina.com>
2017-09-08 09:17:45 +0800

    learn forms

---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Commons/FormElementWithACustomValidator/MyCustomElementBlock.cs
@@ -0,0 +1,10 @@
+﻿using EPiServer.DataAnnotations;
+using EPiServer.Forms.Implementation.Elements.BaseClasses;
+
+namespace EpiserverSiteWithEmpty.Commons.FormElementWithACustomValidator
+{
+    [ContentType(GUID = "{E56EEE33-C330-4F24-8E5C-6A3BD0D4D14A}")]
+    public class MyCustomElementBlock:ValidatableElementBlockBase
+    {
+    }
+}
