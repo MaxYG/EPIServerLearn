4aa20f02b36a88a0b285304649185fa6dde2adec
Tom <yangg@shinetechchina.com>
2017-09-18 14:49:32 +0800

    add RedirectToThankYouPage

---
+++ EpiserverSiteWithEmpty/EpiserverSiteWithEmpty/Commons/RedirectToThankYouPage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using EPiServer.Forms.Core;
+using EPiServer.Forms.Core.Models;
+using EPiServer.Forms.Implementation;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+
+namespace EpiserverSiteWithEmpty.Commons
+{
+    public class RedirectToThankYouPage
+    {
+    }
+
+
+    //todo:T how to use it?
+    [ServiceConfiguration(typeof(IAppendExtraInfoToRedirection))]
+    public class AppendInfoToRedirection : DefaultAppendExtraInfoToRedirection
+    {
+        public override IDictionary<string, object> GetExtraInfo(FormIdentity formIden, Submission submission)
+        {
+            // base.GetExtraInfo(formIden, submission);
+            var info = base.GetExtraInfo(formIden, submission);
+            info.Add("DemoParam","demo value");
+            info.Add("FormSubmissionId",submission.Id);
+            return info;
+        }
+    }
+
+    public class DemoInitializationModule : IInitializableModule, IConfigurableModule
+    {
+        protected ServiceConfigurationContext _ServiceConfigurationContext;
+
+        public void Preload(string[] parameters)
+        {
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+           //_ServiceConfigurationContext.Container.Configure(c=>c.For<IAppendExtraInfoToRedirection>().Use(new AppendInfoToRedirection()));
+            _ServiceConfigurationContext.StructureMap().Configure(c => c.For<IAppendExtraInfoToRedirection>().Use(new AppendInfoToRedirection()));
+
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+           
+        }
+
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            _ServiceConfigurationContext = context;
+        }
+    }
+
+
+
+
+}
